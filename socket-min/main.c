423c5e49d505031445462c31ca1ae684034e6ee6
Aoyama, Shotaro <aosho235@gmail.com>
2018-01-03 04:07:30 +0900

    socket-min

---
+++ socket-min/main.c
@@ -0,0 +1,36 @@
+//=============================================================================
+//  本当に最小限のechoサーバ（ポート9000）
+//  エラーチェックすらなし
+//=============================================================================
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    int accepted;
+    struct sockaddr_in address;
+    char buf[256];
+    int nread;
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&address, 0, sizeof(struct sockaddr_in));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(9000);
+    bind(sock, (struct sockaddr*)&address, sizeof(struct sockaddr_in));
+
+    listen(sock, 5);
+
+    accepted = accept(sock, NULL, NULL);
+
+    while ((nread = read(accepted, buf, sizeof(buf)))) {
+        write(accepted, buf, nread);
+    }
+
+    return 0;
+}
